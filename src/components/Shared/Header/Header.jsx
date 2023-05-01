07f08e7fc641b0c234b3595f4700bf47b0d6a8b9
hnmahamud <hnmahamud@gmail.com>
2023-05-02 02:46:10 +0600

    hader user and login/logout handle

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -1,9 +1,30 @@
-import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
-import React from "react";
+import { Avatar, Button, Dropdown, Navbar, Tooltip } from "flowbite-react";
+import React, { useContext } from "react";
 import logo from "../../../assets/logo.png";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthProviders";
+import { toast } from "react-toastify";
 
 const Header = () => {
+  // Context API
+  const { user, logout } = useContext(AuthContext);
+
+  // Use location for active class
+  const location = useLocation();
+
+  // Logout handler
+  const logoutHandler = () => {
+    logout()
+      .then(() => {
+        toast("Logout Successfully!", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Navbar className="md:w-[50%] md:mx-auto py-4" fluid={true} rounded={true}>
       <Navbar.Brand as={Link} to="/">
@@ -14,26 +35,27 @@ const Header = () => {
       </Navbar.Brand>
 
       <div className="flex md:order-2">
-        <Button gradientDuoTone="cyanToBlue">Login</Button>
-        <Dropdown
-          arrowIcon={false}
-          inline={true}
-          label={
-            <Avatar
-              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-              rounded={true}
-            />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">
-              name@flowbite.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Divider />
-          <Dropdown.Item>Logout</Dropdown.Item>
-        </Dropdown>
+        {user ? (
+          <Tooltip content={user.displayName} placement="left">
+            <Dropdown
+              arrowIcon={false}
+              inline={true}
+              label={<Avatar img={user.photoURL} rounded={true} />}
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">{user.displayName}</span>
+                <span className="block truncate text-sm font-medium">
+                  {user.email}
+                </span>
+              </Dropdown.Header>
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={logoutHandler}>Logout</Dropdown.Item>
+            </Dropdown>
+          </Tooltip>
+        ) : (
+          <Button gradientDuoTone="cyanToBlue">Login</Button>
+        )}
+
         <Navbar.Toggle />
       </div>
 
