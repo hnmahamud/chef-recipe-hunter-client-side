9ad5fd96aa860c3c3d55b856f21bfa547fe967c6
hnmahamud <hnmahamud@gmail.com>
2023-05-02 02:25:13 +0600

    added context api and auth provider

---
+++ src/context/AuthProviders.jsx
@@ -0,0 +1,102 @@
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "../utils/firebase/firebase.config";
+
+// Firebase auth
+const auth = getAuth(app);
+// Create context API
+export const AuthContext = createContext(null);
+
+const AuthProviders = ({ children }) => {
+  // State
+  const [user, setUser] = useState(null);
+  const [fullLoading, setFullLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+  // Registration
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Profile Update
+  const profileUpdate = (updateName, updatePhoto) => {
+    return updateProfile(auth.currentUser, {
+      displayName: updateName,
+      photoURL: updatePhoto,
+    });
+  };
+
+  // Email verification
+  const verificationEmail = () => {
+    return sendEmailVerification(auth.currentUser);
+  };
+
+  // Login
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Login with google
+  const googleSignIn = () => {
+    const googleProvider = new GoogleAuthProvider();
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // Password reset
+  const passwordReset = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  // Logout
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  // State observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      setFullLoading(false);
+
+      return () => {
+        return unsubscribe();
+      };
+    });
+  }, []);
+
+  // Data sent as context API
+  const authInfo = {
+    loading,
+    setLoading,
+    setFullLoading,
+    fullLoading,
+    user,
+    createUser,
+    profileUpdate,
+    verificationEmail,
+    loginUser,
+    googleSignIn,
+    passwordReset,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProviders;
