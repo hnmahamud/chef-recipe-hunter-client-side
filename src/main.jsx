379fe13feac428d20ac5760078d325f8cb5af575
hnmahamud <hnmahamud@gmail.com>
2023-05-02 02:22:42 +0600

    added firebase and tostify

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes.jsx";
+import AuthProviders from "./context/AuthProviders";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}></RouterProvider>
+  <AuthProviders>
+    <RouterProvider router={router}></RouterProvider>
+    <ToastContainer />
+  </AuthProviders>
 );
---
+++ src/utils/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5IcrSbcXh_Tksul3xIoVyVgSRIWVRbuI",
+  authDomain: "chef-recipe-hunter-c31f0.firebaseapp.com",
+  projectId: "chef-recipe-hunter-c31f0",
+  storageBucket: "chef-recipe-hunter-c31f0.appspot.com",
+  messagingSenderId: "161492650202",
+  appId: "1:161492650202:web:4317ffbaf40402213b3de3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
