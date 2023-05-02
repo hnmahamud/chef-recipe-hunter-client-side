01d7386cf694e1885b3c4bd226e99ada18ad0048
hnmahamud <hnmahamud@gmail.com>
2023-05-02 16:05:47 +0600

    footer update

--- src/components/Shared/Footer/MyFooter.jsx
+++ src/components/Shared/Footer/MyFooter.jsx
@@ -1,24 +1,64 @@
 import { Footer } from "flowbite-react";
 import React from "react";
-import logo from "../../../assets/logo.png";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaGithub,
+  FaDribbble,
+} from "react-icons/fa";
 
 const MyFooter = () => {
   return (
-    <Footer container={true}>
-      <div className="w-full text-center">
-        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+    <Footer className="bg-black rounded-none" bgDark={true}>
+      <div className="w-full">
+        <div className="grid w-full grid-cols-2 gap-8 py-8 px-6 md:grid-cols-4">
           <div>
-            <Footer.Brand href="#" src={logo} name="Cookiteer" />
+            <Footer.Title title="Cookiteer" />
+            <Footer.LinkGroup col={true}>
+              <Footer.Link href="#">About</Footer.Link>
+              <Footer.Link href="#">Careers</Footer.Link>
+              <Footer.Link href="#">Brand Center</Footer.Link>
+              <Footer.Link href="#">Blog</Footer.Link>
+            </Footer.LinkGroup>
           </div>
-          <Footer.LinkGroup className="flex gap-4">
-            <Footer.Link href="#">About</Footer.Link>
-            <Footer.Link href="#">Privacy Policy</Footer.Link>
-            <Footer.Link href="#">Licensing</Footer.Link>
-            <Footer.Link href="#">Contact</Footer.Link>
-          </Footer.LinkGroup>
+          <div>
+            <Footer.Title title="help center" />
+            <Footer.LinkGroup col={true}>
+              <Footer.Link href="#">Discord Server</Footer.Link>
+              <Footer.Link href="#">Twitter</Footer.Link>
+              <Footer.Link href="#">Facebook</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="legal" />
+            <Footer.LinkGroup col={true}>
+              <Footer.Link href="#">Privacy Policy</Footer.Link>
+              <Footer.Link href="#">Licensing</Footer.Link>
+              <Footer.Link href="#">Terms & Conditions</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="download" />
+            <Footer.LinkGroup col={true}>
+              <Footer.Link href="#">iOS</Footer.Link>
+              <Footer.Link href="#">Android</Footer.Link>
+              <Footer.Link href="#">Windows</Footer.Link>
+              <Footer.Link href="#">MacOS</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
         </div>
-        <Footer.Divider />
-        <Footer.Copyright href="#" by="Cookiteer™" year={2022} />
+        <div className="w-full bg-gray-700 py-6 px-4 sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="Cookiteer™" year={2023} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={FaFacebook} />
+            <Footer.Icon href="#" icon={FaInstagram} />
+            <Footer.Icon href="#" icon={FaTwitter} />
+            <Footer.Icon href="#" icon={FaGithub} />
+            <Footer.Icon href="#" icon={FaDribbble} />
+          </div>
+        </div>
       </div>
     </Footer>
   );
